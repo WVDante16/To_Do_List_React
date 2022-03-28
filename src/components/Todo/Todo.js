43095268efba689a44d0ebd6e9c21fcb9d35b174
WVDante16 <walbertozekrom@gmail.com>
2022-03-28 10:54:49 -0600

    Update todolist

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -29,11 +29,59 @@ class Todo extends Component {
         }
     }
 
+    handleOnChange = e => {
+        const {target:{value}} = e;
+        this.setState({
+            task: value
+        })
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault();
+
+        if (this.state.task.trim() != '') {
+            this.setState({
+                task: '',
+                items: [
+                    ...this.state.items,
+                    {
+                        id: uuidv4(),
+                        task: this.state.task,
+                        complete: false
+                    }
+                ]
+            })
+        }
+    }
+
     render() {
-        console.log(this.state-items);
+        console.log(this.state.items);
+        //console.log(this.state.task);
         return (
             <div className="Todo">
                 <h1>Nueva Tarea</h1>
+
+                <form onSubmit = {this.handleOnSubmit}>
+                     <input
+                        type = "text"
+                        value = {this.state.task}
+                        onChange = {this.handleOnChange}
+                     />
+                </form>
+
+                <ul>
+                    {
+                        this.state.items.map((item) => (
+                            <li>
+                                {item.task}
+                                 <div>
+                                     <span>Icono check</span>
+                                     <span>Icono basurero</span>
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
