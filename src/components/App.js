9e3c41ce9ebd5b92d257153be084030a320f8fa4
WVDante16 <walbertozekrom@gmail.com>
2022-04-25 11:00:26 -0500

    Pomodoro

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import Home from './Home/Home';
+//import Home from './Home/Home';
 import Header from '../shared/layout/Header';
 import './App.css';
 import Footer from '../shared/layout/Footer';
 import Content from '../shared/layout/Content';
-import Todo from './Todo/Todo';
+//import Todo from './Todo/Todo';
+import Timer from './Pomodoro/Timer';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header title="Header" url="https://github.com/" />
       
       <Content>
-        <Todo />
+        <Timer />
       </Content>
       
       <Footer />
